ede863b89c60101ad381fd704cad2219d9091efc
Daniel Peña <digrape07@gmail.com>
2018-07-26 21:50:52 -0400

    Comment class removed.

--- App.js
+++ App.js
@@ -1,30 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import Home from './app/views/Home.js';
-/*
-export class Home extends React.Component{
-  constructor(props){
-      super(props);
-      this.state = {
-          timesClicked:0,
-      }
-  }
-  updateClicker = () =>{
-      this.setState(prevState =>{
-          return { timesClicked: prevState.timesClicked + 1 };
-      });
-  }
-  render(){
-      return(
-          <View>
-              <Text>
-                  {this.props.text}: {this.state.timesClicked}
-              </Text>
-              <Button title="Click" onPress={ () => {this.updateClicker} } />
-          </View>
-      );
-  }
-}*/
 
 export default class App extends React.Component {
   render() {
